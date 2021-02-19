449be54d506e045085034ac44078a2119db52eb8
Erik Bolager <erik.bolager@gmail.com>
2021-02-19 16:07:09 +0100

    Delete Code/NF directory

--- Code/NF/setup.py
+++
@@ -1,5 +0,0 @@
-from setuptools import setup, find_packages
-setup(
-    name = 'normalizing_flow',
-    packages = find_packages(),
-)
--- Code/NF/src/utils.py
+++
@@ -1,6 +0,0 @@
-def invert_permutation(permutation):
-    inv_perm = [0] * len(permutation)
-
-    for i in  range(len(permutation)):
-        inv_perm[permutation[i]] = i
-    return inv_perm
